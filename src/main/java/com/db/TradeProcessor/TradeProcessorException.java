1200b89bc50be561e67a5d8305415f4339b6651a
SatyaPrasadKotriki <88182627+SatyaPrasadKotriki@users.noreply.github.com>
2021-08-01 20:20:00 +0530

    Removed unused import, field moved to local

--- src/main/java/com/db/TradeProcessor/TradeProcessorException.java
+++ src/main/java/com/db/TradeProcessor/TradeProcessorException.java
@@ -2,11 +2,7 @@ package com.db.TradeProcessor;
 
 public class TradeProcessorException extends Exception {
 
-    private final String errorMessage;
-
     public TradeProcessorException(String errorMessage) {
         super(errorMessage);
-        this.errorMessage = errorMessage;
-
     }
 }
--- src/main/java/com/db/TradeProcessor/controller/TradeProcessorController.java
+++ src/main/java/com/db/TradeProcessor/controller/TradeProcessorController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
